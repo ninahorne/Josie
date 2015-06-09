1b0aee9b6e9875aaa2902d5df1f6797c908d0b3e
ninahorne <nina.horne1221@gmail.com>
2015-06-09 16:10:17 -0500

    Blog Started

---
+++ Blog.Core/Blog.Core/IBlogRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core
+{
+    interface IBlogRepository
+    {
+        public interface IBlogRepository
+        {
+            IList<Post> Posts(int pageNo, int pageSize);
+            int TotalPosts();
+        }
+    }
+}
---
+++ Blog.Core/Blog.Core/Mappings/PostMap.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using Blog.Core.Objects;
+
+
+
+namespace Blog.Core.Mappings
+{
+    public class PostMap: ClassMap<Post>
+    {
+        public PostMap()
+        {
+            Id(x => x.Id);
+
+            Map(x => x.Title)
+                .Length(500)
+                .Not.Nullable();
+
+            Map(x => x.ShortDescription)
+                .Length(5000)
+                .Not.Nullable();
+
+            Map(x => x.Description)
+                .Length(5000)
+                .Not.Nullable();
+
+            Map(x => x.Meta)
+                .Length(1000)
+                .Not.Nullable();
+
+            Map(x => x.UrlSlug)
+                .Length(200)
+                .Not.Nullable();
+
+            Map(x => x.Published)
+                .Not.Nullable();
+
+            Map(x => x.PostedOn)
+                .Not.Nullable();
+
+            Map(x => x.Modified);
+
+            References(x => x.Category)
+                .Column("Category")
+                .Not.Nullable();
+
+            HasManyToMany(x => x.Tags)
+                .Table("PostTagMap");
+        }
+    }
+    public class CategoryMap : ClassMap<Category>
+    {
+        public CategoryMap()
+        {
+            Id(x => x.Id);
+
+            Map(x => x.Name)
+                .Length(50)
+                .Not.Nullable();
+
+            Map(x => x.UrlSlug)
+                .Length(50)
+                .Not.Nullable();
+
+            Map(x => x.Description)
+                .Length(200);
+
+            HasMany(x => x.Posts)
+                .Inverse()
+                .Cascade.All()
+                .KeyColumn("Category");
+        }
+    }
+    public class TagMap : ClassMap<Tag>
+    {
+        public TagMap()
+        {
+            Id(x => x.Id);
+
+            Map(x => x.Name)
+                .Length(50)
+                .Not.Nullable();
+
+            Map(x => x.UrlSlug)
+                .Length(50)
+                .Not.Nullable();
+
+            Map(x => x.Description)
+                .Length(200);
+
+            HasManyToMany(x => x.Posts)
+                .Cascade.All().Inverse()
+                .Table("PostTagMap");
+        }
+    }
+}
